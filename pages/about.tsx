83e3d0e8a9b2fcf76c9791de0deea4dd94d2f1e0
yunusp <yunuspoonawala52@gmail.com>
2023-03-08 20:34:27 +0530

    drop shadow and things

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,9 +6,9 @@ export default function About() {
             <Head><title>Rental - About</title></Head>
             <div className="bg-blue-500 h-full pt-4">
                 <div className="w-7/12" id={styles["about-item"]}>
-                    <div><h1 className="text-center font-bold text-5xl">About us</h1> <br />
+                    <div><h1 className="text-center font-bold text-5xl drop-shadow-lg">About us</h1> <br />
                         <span id={styles["about-text"]}>
-                            <p>This website was made by <i>Yunus Poonawala (202000348)</i> for the final year project. Please have fun using it.</p>
+                            <p>This website was made by <i className="hover:font-bold hover:drop-shadow-lg">Yunus Poonawala (202000348)</i> for the final year project. Please have fun using it.</p>
                             <p>More things will go here probably. Decide what they will be later.</p>
                             <p>What follows is nonsense:</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit aliquam etiam erat velit. Molestie ac feugiat sed lectus vestibulum. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Cum sociis natoque penatibus et magnis dis parturient montes. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Facilisis sed odio morbi quis commodo. Posuere morbi leo urna molestie. Pretium viverra suspendisse potenti nullam. Id leo in vitae turpis massa sed elementum tempus. Vitae turpis massa sed elementum tempus egestas sed sed. Nam libero justo laoreet sit amet cursus sit amet. Pellentesque sit amet porttitor eget dolor morbi. Lectus nulla at volutpat diam.</p>
