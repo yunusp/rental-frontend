254ba910438d9e27c25b36f588d30b1a4cab0aca
yunusp <yunuspoonawala52@gmail.com>
2023-03-10 01:31:35 +0530

    bottom margin + todo list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import CarCard from "../components/ui/CarCard";
-import { CarCardPropObject, CarCardProps } from "../interfaces/carCardProps";
+import { CarCardPropObject } from "../interfaces/carCardProps";
 export default function Index(props: CarCardPropObject) {
     const prop = props.cars;
     return (
         <>
             <Head><title>Rental - Home</title></Head>
-            <div className="h-full min-h-screen">
+            <div className="h-full min-h-screen pb-2">
                 <div className="text-center text-5xl font-title pt-4 font-bold drop-shadow-lg">Browse our selection of cars</div> <br />
                 <div className="flex justify-center flex-wrap [&>*]:m-1">
-                    {prop.map(data => <CarCard name={data.name} dt={data.dt} price={data.price} />)}
+                    {prop.map(data => <CarCard name={data.name} dt={data.dt} price={data.price} description={data.description} />)}
                 </div>
             </div>
         </>
@@ -18,7 +18,7 @@ export default function Index(props: CarCardPropObject) {
 
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: any) {
     const jsonData = await fetch("http://localhost:3000/info.json", { method: "get" });
     const data: CarCardPropObject = await jsonData.json();
     return {
