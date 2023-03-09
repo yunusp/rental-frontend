3d436ce4bd85495629c3d1aa39bfabfd1336b5f3
yunusp <yunuspoonawala52@gmail.com>
2023-03-10 01:01:51 +0530

    get static props

--- components/ui/CarCard/index.tsx
+++ components/ui/CarCard/index.tsx
@@ -6,12 +6,12 @@ export default function CarCard(props: CarCardProps) {
     const carYear: string = (props.year ?? "???").toString();
     const carDesc: string = props.description ?? "No description given";
     return (
-        <div id={styles.card} className=" p-1 border border-black rounded-lg drop-shadow w-64 h-64 flex flex-col content-between justify-start items-center">
+        <a href="https://www.google.com" id={styles.card} className=" p-1 border border-black rounded-lg drop-shadow w-64 h-64 flex flex-col content-between justify-start items-center">
             <img src={carImgUrl} alt={carDesc} className="h-1/2" draggable={false} />
             <p className="text-2xl text-center truncate w-60">{props.name}</p>
             <p className="text-center truncate w-60">{props.dt}</p>
             <p className="text-xl">₹{props.price}</p>
             <p className="text-center">{carDesc}</p>
-        </div>
+        </a>
     );
 }
