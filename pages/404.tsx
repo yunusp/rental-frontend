904df4a2cdac8210d13e55ca4d2a31375ce997f4
yunusp <yunuspoonawala52@gmail.com>
2023-03-10 03:41:09 +0530

    bottom 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,8 +7,10 @@ export default function FourOhFour() {
     <div className="h-full min-h-screen pb-2">
       <div className="flex pt-8 items-center h-screen text-8xl font-bold flex-col">
         <div>404</div>
-        <div>Not found</div>
-        <div className='mt-4 text-3xl'>That's all we know :(</div>
+        <div>Not Found</div>
+        <div className='mt-4 text-3xl'>That's all we know.</div>
+        <div className='mt-8 text-8xl'>🥺</div>
+
       </div>
     </div>
   </>);
