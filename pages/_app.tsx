8ec18df61154e135493daa485f15a6a44ce1ee32
yunusp <yunuspoonawala52@gmail.com>
2023-03-10 07:48:50 +0530

    animations?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import BaseLayout from '../components/Layouts/Base'
+import { useEffect } from 'react';
+import AOS from "aos";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <BaseLayout>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import Head from "next/head";
+import 'aos/dist/aos.css';
 import CarCard from "../components/ui/CarCard";
 import { CarCardPropObject } from "../interfaces/carCardProps";
 export default function Index(props: CarCardPropObject) {
     const prop = props.cars;
     return (
         <>
-            <Head><title>Rental - Home</title></Head>
+            <Head>
+                <title>Rental - Home</title>
+
+            </Head>
             <div className="h-full min-h-screen pb-2">
                 <div className="text-center text-5xl font-title pt-4 font-bold drop-shadow-lg">Browse our selection of cars</div> <br />
                 <div className="flex justify-center flex-wrap [&>*]:m-1">
-                    {prop.map((data, index) => <CarCard id={index} key={index} name={data.name} dt={data.dt} price={data.price} description={data.description} picture={data.picture} />)}
+                    {prop.map((data, index) => <CarCard data-aos="fade-in" id={index} key={index} name={data.name} dt={data.dt} price={data.price} description={data.description} picture={data.picture} />)}
                 </div>
             </div>
         </>
