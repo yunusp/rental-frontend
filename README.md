64d58af18e44a0787821c44f43f2333db0455503
yunusp <yunuspoonawala52@gmail.com>
2023-03-25 19:55:14 +0530

    sign in page

--- README.md
+++ README.md
@@ -12,5 +12,8 @@
 - [ ] Come up with consistent colors
 - [ ] Make routes be more sensible
 
+
+## Notes
+To use jwt, generate 1 at login page and then send it as a hidden form field or something every time another form needs filling.
 # Credits
 My beloved room mate and friend [alphanoobie](https://github.com/alphanoobie) -- Whose contributions, which lacked in quantity, made up for themselves in profundity.
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
 import styles from "../styles/sign.module.css"
 export default function SignIn() {
+    const [formVal, setFormVal] = useState(0);
     async function handleSubmit(e: any) {
         e.preventDefault();
 
@@ -19,13 +22,35 @@ export default function SignIn() {
         };
         const response = await fetch(endpoint, options);
 
-
+        setFormVal(response.status);
     }
+    let status = undefined;
+    switch (formVal) {
+        case 200:   // all good. allow sign in
+            status = (<div className="text-green-800 text-center text-2xl">
+                Sign in successful. <br /> This does nothing for now. <br /> Please be patient.
+            </div>);
+            break;
+        //! perhaps the next two should be the same.
+        case 403:   // no such user
+            status = (<div className="text-red-800 font-bold text-center text-2xl">
+                User name not found! Perhaps you wish to <Link className="text-blue-600" href={"/signup"}>Sign up</Link>?
+            </div>);
+            break;
+        case 401:   //incorrect password
+            status = (<div className="text-red-600 font-bold text-center text-2xl">
+                Incorrect user name or password!
+            </div>);
+        default:
+            break;
+    }
+
     return (
         <>
             <Head><title>Rental - Sign in</title></Head>
             <div className="h-full min-h-screen pb-2">
                 <div className="text-center text-5xl font-title pt-4 font-bold drop-shadow-lg">Sign in</div> <br />
+                {status}
                 <div className="flex justify-center items-center flex-col flex-wrap [&>*]:m-1">
                     <form onSubmit={handleSubmit} id={styles.form} className=" flex justify-center items-center flex-col flex-wrap text-lg [&>*]:m-1">
                         <input type="text" name="uname" id="uname" placeholder="User Name" className="h-8 p-4" /> <br />
