5152f49cacddc3c2ff24e60bce4289e5f218f2f7
yunusp <yunuspoonawala52@gmail.com>
2023-03-26 17:58:37 +0530

    signup works works

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ChangeEvent, FormEvent, MutableRefObject, SetStateAction, useRef, useState } from "react";
 import styles from "../styles/sign.module.css"
 export default function SignUp() {
+    const router = useRouter();
     const [pnumVal, setPNum] = useState('6000000000');
     function pnumChangeHandler(e: any) {
         setPNum(e.target.value)
@@ -9,15 +11,14 @@ export default function SignUp() {
     const [formVal, setFormVal] = useState(0);
     const fileRef: MutableRefObject<any> = useRef(null);
     async function handleSubmit(e: any) {
-        //! handle same password validation here
-        function _arrayBufferToBase64( buffer: ArrayBuffer ) {
+        function _arrayBufferToBase64(buffer: ArrayBuffer) {
             var binary = '';
-            var bytes = new Uint8Array( buffer );
+            var bytes = new Uint8Array(buffer);
             var len = bytes.byteLength;
             for (var i = 0; i < len; i++) {
-                binary += String.fromCharCode( bytes[ i ] );
+                binary += String.fromCharCode(bytes[i]);
             }
-            return window.btoa( binary );
+            return window.btoa(binary);
         }
         e.preventDefault();
 
@@ -25,6 +26,12 @@ export default function SignUp() {
 
         const file_data: ArrayBuffer = await fileRef.current.files[0].arrayBuffer();
         const b64 = _arrayBufferToBase64(file_data);
+
+        // handle same password validation here so that we do not send thing we dont need to.
+        if (e.target.passwd.value !== e.target.cpasswd.value) {
+            alert("Passwords do not match!");
+            return;
+        }
 
         const data = {
             uname: e.target.uname.value,
@@ -43,7 +50,10 @@ export default function SignUp() {
             body: new URLSearchParams(data),
         };
         const response = await fetch(endpoint, options);
-
+        if (response.status == 201) {
+            localStorage.setItem("username", e.target.uname.value);
+            router.push("/signin");
+        }
         setFormVal(response.status);
     }
     return (
@@ -54,6 +64,7 @@ export default function SignUp() {
                 <div className="flex justify-center items-center flex-col flex-wrap [&>*]:m-1">
                     <form onSubmit={handleSubmit} id={styles.form} className=" flex justify-center items-center flex-col flex-wrap text-lg [&>*]:m-1">
                         <input required={true} type="text" name="uname" id="uname" placeholder="User Name" className="h-8 p-4" /> <br />
+                        {formVal == 403 ? <div className="text-xl text-red-700 font-bold">user name already exists</div> : <div></div>}
                         <input required={true} type="email" name="email" id="email" placeholder="Email" className="h-8 p-4" /> <br />
                         <label htmlFor="pnum">Phone number: </label>
                         <input required={true} type="range" min={6000000000} max={9999999999} value={pnumVal} defaultValue={pnumVal} onChange={e => pnumChangeHandler(e)} name="pnum" id="pnum" placeholder="Phone number" className="h-8 p-4" />
