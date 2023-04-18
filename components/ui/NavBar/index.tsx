a4e68516aff0ca63ca7ab7edeb5dfa4ab891a4c3
yunusp <yunuspoonawala52@gmail.com>
2023-04-18 07:10:11 +0530

    shows welcome on main page

--- components/ui/NavBar/index.tsx
+++ components/ui/NavBar/index.tsx
@@ -1,17 +1,33 @@
 import styles from "./header.module.css";
 import Link from "next/link";
+import Cookies from 'js-cookie';
+import { useEffect, useState } from "react";
+
 export default function NavBar() {
+    let [userCookie, setUserCookie] = useState("");
+    useEffect(() => {
+        let cc = Cookies.get("user");
+        setUserCookie(cc ?? "");
+    }, []);
+    let output;
+    if (userCookie !== "") {
+        output = (<span id={styles.sign}>
+            <Link className={styles.hclick} href="/">Welcome, {userCookie}</Link>
+        </span>);
+    } else {
+        output = (<span id={styles.sign}>
+            <Link className={styles.hclick} href="/signin">Sign in</Link>
+            <Link className={styles.hclick} href="/signup">Sign up</Link>
+        </span>);
+    }
     return (
-            <div id={styles.header}>
-                <span id={styles.site}>
-                    <Link className={styles.hclick} href="/">Home</Link>
-                    <Link className={styles.hclick} href="/about">About</Link>
-                    <Link className={styles.hclick} href="/contact">Contact</Link>
-                </span>
-                <span id={styles.sign}>
-                    <Link className={styles.hclick} href="/signin">Sign in</Link>
-                    <Link className={styles.hclick} href="/signup">Sign up</Link>
-                </span>
-            </div>
+        <div id={styles.header}>
+            <span id={styles.site}>
+                <Link className={styles.hclick} href="/">Home</Link>
+                <Link className={styles.hclick} href="/about">About</Link>
+                <Link className={styles.hclick} href="/contact">Contact</Link>
+            </span>
+            {output}
+        </div>
     );
 }
