068fa4937aa2feb96a31ccce78658232545d6327
yunusp <yunuspoonawala52@gmail.com>
2023-04-18 09:02:53 +0530

    put booking behind auth gate

--- pages/book/[carid].tsx
+++ pages/book/[carid].tsx
@@ -1,10 +1,24 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import styles from "../../styles/book.module.css";
+import { useEffect, useState } from "react";
+import Cookies from 'js-cookie';
+import { useCookies } from 'react-cookie';
 
 export default function Book() {
     const router = useRouter();
     const { carid } = router.query;
+
+    let [userCookie, setUserCookie] = useState("null");
+    useEffect(() => {
+        let cc = Cookies.get("user");
+        setUserCookie(cc ?? "");
+    }, []);
+
+    if (userCookie === "") {
+        router.push("/signin");
+    }
+
     return (
         <>
             <Head><title>Rental - {carid}</title></Head>
@@ -12,8 +26,6 @@ export default function Book() {
                 <div className="text-center text-5xl font-title pt-4 font-bold drop-shadow-lg">Book car {carid}</div> <br />
                 <div id={styles.blinker} className="text-center text-3xl font-bold font-body">This is not complete! <br /> Please do not assume , even if it were for a for a passing moment, that this is final.</div>
                 <div className="flex justify-center flex-wrap [&>*]:m-1">
-                    <p className="text-5xl text-center">Hark! This page is not for all eyes! <br />Please ensure that you have signed in before looking further.</p>
-                    <p className="text-4xl">Unforseen consequences await those who do not!</p>
                 </div>
             </div>
         </>
