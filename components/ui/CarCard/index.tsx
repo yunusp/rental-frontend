1d63475db839393ed62b5cc7b4b7c5a3e99614e2
yunusp <yunuspoonawala52@gmail.com>
2023-04-22 11:17:31 +0530

    cars already borrowed don't show up on main page

--- components/ui/CarCard/index.tsx
+++ components/ui/CarCard/index.tsx
@@ -3,17 +3,18 @@ import { CarCardProps } from "../../../interfaces/carCardProps"
 import styles from "./CarCard.module.css";
 
 export default function CarCard(props: CarCardProps) {
+
     const carImgUrl: string = props.picture ?? "/favicon.ico";
     const carYear: string = (props.yop ?? "???").toString();
     const carDesc: string = props.desc ?? "No description given";
     const link: string = (props._id["$oid"] ?? "/404").toString();
-    return (
-        <Link href={`/book/${link}`} data-aos-duration="2000" data-aos="fade-left" draggable={false} id={styles.card} className=" p-1 border border-black rounded-lg drop-shadow w-64 h-64 flex flex-col content-between justify-start items-center">
+    return props.borrower_id ? (<></>) :
+        (<Link href={`/book/${link}`} data-aos-duration="2000" data-aos="fade-left" draggable={false} id={styles.card} className=" p-1 border border-black rounded-lg drop-shadow w-64 h-64 flex flex-col content-between justify-start items-center">
             <img src={carImgUrl} alt={carDesc} className="h-1/2" draggable={false} />
             <p className="text-2xl text-center truncate w-60">{props.name}</p>
             <p className="text-center truncate w-60">{props.dt}</p>
             <p className="text-xl">₹{props.price}</p>
             <p className="text-center">{carDesc}</p>
-        </Link>
-    );
+        </Link>)
+
 }
