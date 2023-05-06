d8a9c0bef666cc46a775056c06bbdee5f53f3a46
yunusp <yunuspoonawala52@gmail.com>
2023-05-06 17:35:46 +0530

    remove vars

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -12,10 +12,10 @@ export default function SignUp() {
     const fileRef: MutableRefObject<any> = useRef(null);
     async function handleSubmit(e: any) {
         function _arrayBufferToBase64(buffer: ArrayBuffer) {
-            var binary = '';
-            var bytes = new Uint8Array(buffer);
-            var len = bytes.byteLength;
-            for (var i = 0; i < len; i++) {
+            let binary = '';
+            let bytes = new Uint8Array(buffer);
+            let len = bytes.byteLength;
+            for (let i = 0; i < len; i++) {
                 binary += String.fromCharCode(bytes[i]);
             }
             return window.btoa(binary);
